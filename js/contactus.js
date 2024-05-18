7cc15d68074f4a78024b6ccae901d303d4fc46c1
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-05-19 00:02:19 +0530

    added changes

--- js/contactus.js
+++ js/contactus.js
@@ -1,13 +1,11 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    // Collecting form data
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
 
-    // Creating an object to store form data
     const formData = {
         name: name,
         email: email,
@@ -15,15 +13,11 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         message: message
     };
 
-    // Printing form data to console
     console.log('Form Data Submitted:', formData);
 
-    // Storing form data in local storage
     localStorage.setItem('contactFormData', JSON.stringify(formData));
 
-    // Displaying a confirmation message
     alert('Thank you for your message. We will get back to you soon!');
     
-    // Resetting the form
     this.reset();
 });
